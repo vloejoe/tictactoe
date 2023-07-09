7334836f8fa9691ff2009e45c45532286b9b6624
vloejoe <81408505+vloejoe@users.noreply.github.com>
2023-07-09 19:02:49 +0200

    clickable game ! it display only X...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { Children } from 'react';
+import { useState } from 'react';
 import './style.scss';
-import Square from './component/Square';
 import Board from './component/Board';
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  const onBtnclick = () => {
+    setCounter(currentCounter => {
+      return currentCounter + 1;
+    });
+  };
+
   return (
     <div className="app">
+      <button onClick={onBtnclick}>Click me</button>
+      <div>{counter}</div>
       <Board />
     </div>
   );
--- src/component/Square.jsx
+++ src/component/Square.jsx
@@ -1,6 +1,6 @@
-const Square = ({ value, children }) => {
+const Square = ({ value, onClick }) => {
   return (
-    <button type="button" className="square">
+    <button type="button" className="square" onClick={onClick}>
       {value}
     </button>
   );
